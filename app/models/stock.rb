9efc388cec87a51c2d08c59a50df03d5d67bf438
eden-bs <edenbs1997@gmail.com>
2025-04-09 22:03:56 +0300

    Final Project

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,12 +1,32 @@
+
+
+# response = HTTParty.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alphavantage[:sandbox_api_key]}")
 require 'httparty'
  
 class Stock < ApplicationRecord
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+    validates :name, :ticker, presence: true
+    def self.company_lookup(ticker_symbol)
+        response = HTTParty.get("https://www.alphavantage.co/query?function=OVERVIEW&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alphavantage[:sandbox_api_key]}")
+        if response.code == 200
+          response.parsed_response['Name']
+        else
+          nil
+        end
+    end
+ 
     def self.new_lookup(ticker_symbol)
         response = HTTParty.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alphavantage[:sandbox_api_key]}")
         if response.code == 200 && response.parsed_response['Global Quote']
-            response.parsed_response['Global Quote']['05. price']
+          last_price = response.parsed_response['Global Quote']['05. price']
+          name = company_lookup(ticker_symbol)
+          new(ticker: ticker_symbol, name: name, last_price: last_price)
         else
-            nil
+          nil
         end
+    end
+    def self.check_db(ticker_symbol)
+      where(ticker: ticker_symbol).first
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,25 @@
 Rails.application.routes.draw do
+
+  resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   root "welcome#index"
+  get "my_portfolio", to: "users#my_portfolio"
+  get "search_stock", to: "stocks#search"
+  get 'my_friends', to: 'users#my_friends'
+  get "search_friend", to: "users#search"
+  resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
+
+
+
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
+
   end
   # get '/users/sign_out', to: 'devise/sessions#destroy'
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
